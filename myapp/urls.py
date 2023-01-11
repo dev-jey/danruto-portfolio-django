1f109ee3e41bcd8b45e0d9f29ae939eed0f3d30b
dev-jey <savalijey@gmail.com>
2023-01-11 17:42:06 +0300

    Add case studies

--- myapp/urls.py
+++ myapp/urls.py
@@ -6,7 +6,7 @@ from django.shortcuts import redirect
 urlpatterns = [
     path('', views.get_home, name='home'),
     path('about/', views.get_about, name='about'),
-    path('logo/', views.get_logos, name='logo'),
+    path('case-studies/', views.get_logos, name='logo'),
     path('portfolio/', views.get_portfolio, name='portfolio'),
     path('contact/', views.get_contact, name='contact')
 ]
